55b94dffa934a9907b9319073f62f612b7dac7cc
PLsergent <papa.louis59@gmail.com>
2020-03-25 20:40:10 +0100

    JSX draw test

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -8,12 +8,35 @@ import JXG from "jsxgraph";
 export default {
   name: "VueCanvas",
   mounted() {
-    JXG.JSXGraph.initBoard("vue-canvas", {
-      boundingbox: [-5, 5, 5, -5],
-      axis: true
+    var board = JXG.JSXGraph.initBoard('vue-canvas', { 
+      boundingbox: [-5, 5, 5, -5], axis:true
     });
+
     var elt = document.getElementById("vue-canvas_licenseText");
     elt.style.display = "none";
+
+    // Macro function plotter
+    function addCurve(board, func, atts) {
+        var f = board.create('functiongraph', [func], atts);
+        return f;
+    }
+    
+    // Simplified plotting of function
+    function plot(func, atts) {
+      if (atts==null) {
+          return addCurve(board, func, {strokewidth:2});
+      } else {
+          return addCurve(board, func, atts);
+      }    
+    }
+
+    var p = board.create('point', [1,1], {style:6, name:'p'}); 
+
+    function f(x) {
+      return Math.cos(x)*p.Y();
+    }
+
+    plot(f);
   }
 };
 </script>
