ebc63d2a3956d5e85f44af91dada5f8fbdde5331
39291696 <birnagerald.pro@gmail.com>
2020-03-25 17:59:01 +0100

    add: Canvas component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <VueCanvas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueCanvas from "./components/Canvas.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    VueCanvas
   }
-}
+};
 </script>
 
 <style>
